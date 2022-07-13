455e1446711eebcdb24d54ec96cf831c824a37a2
redbor24 <redbor24@gmail.com>
2022-07-13 17:11:31 +0300

    Add frontend

--- yandex_afisha/settings.py
+++ yandex_afisha/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from environs import Env
@@ -27,6 +28,8 @@ SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
 DEBUG = env.bool('DEBUG', True)
 
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Application definition
 
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'yandex_afisha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- yandex_afisha/views.py
+++ yandex_afisha/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    print('Кто-то зашёл на главную!')
-    return HttpResponse('<h2>Здесь будет карта</h2>')
+    # return render(request, 'index.html', context)
+    return render(request, 'index.html')
