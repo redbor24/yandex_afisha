68a7be41aa75bf9f2bcdf6428eb45985ee22079d
redbor24 <redbor24@gmail.com>
2022-07-14 16:56:35 +0300

    В админке реализовано редактирование списка картинок локации из самой локации

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
+class ImagesInline(admin.TabularInline):
+    model = Image
+    fields = ('image', 'num', )
+    verbose_name = 'Изображение'
+    verbose_name_plural = 'Изображения'
+    extra = 0
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ImagesInline]
 
 
 @admin.register(Image)
