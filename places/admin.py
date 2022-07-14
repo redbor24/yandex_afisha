06ebd6525489af3ae60c0db7a223ee69792ee378
redbor24 <redbor24@gmail.com>
2022-07-14 17:00:37 +0300

    Реализовано отображение превьюшек изображений локации на форме редактирования локации

--- places/admin.py
+++ places/admin.py
@@ -1,13 +1,27 @@
+from django.utils.html import format_html, mark_safe
 from django.contrib import admin
 from places.models import Place, Image
 
 
 class ImagesInline(admin.TabularInline):
     model = Image
-    fields = ('image', 'num', )
+    fields = ('image', 'preview_image', 'num', )
     verbose_name = 'Изображение'
     verbose_name_plural = 'Изображения'
     extra = 0
+
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        return format_html(
+            '{}',
+            mark_safe(
+                '<img src="{url}" height={height} />'.format(
+                    url=obj.image.url,
+                    height=200,
+                )
+            )
+        )
 
 
 @admin.register(Place)
